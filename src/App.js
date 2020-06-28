39eddd7ffd1b0b4029c978bb2e83c7fab630e1b3
Bahati Robben <bahatiroben@gmail.com>
2020-06-28 23:15:12 +0200

    view users

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import React from 'react'
+import UserTable from './tables/UserTable'
 
 const App = () => {
+  const usersData = [
+    { id: 1, name: 'Tania', username: 'floppydiskette' },
+    { id: 2, name: 'Craig', username: 'siliconeidolon' },
+    { id: 3, name: 'Ben', username: 'benisphere' },
+  ]
+
+  const [users, setUsers] = useState(usersData)
   return (
     <div className="container">
-      <h1>User Attendance</h1>
+      <h1>User Attendance using Hooks</h1>
       <div className="flex-row">
         <div className="flex-large">
           <h2>Add user</h2>
         </div>
         <div className="flex-large">
-          <h2>View users</h2>
+          <h2>Users</h2>
+          <UserTable users={users} />
         </div>
       </div>
     </div>
